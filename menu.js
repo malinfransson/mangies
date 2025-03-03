4aae3f29d0e4d70c751ce6b96b5b93e7fff55f90
malinfransson <159783622+malinfransson@users.noreply.github.com>
2025-03-03 17:42:52 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,3 +4,8 @@ function opennav ()
 
 function closenav () 
 {document.getElementById ("menu").style.width="0%";}
+
+function toggleActive(elementId) {
+    document.getElementById(elementId).classList.toggle("active");
+}
+
